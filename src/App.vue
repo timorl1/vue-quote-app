a3e03efdf8a6278d5d622a439e7f9891c06fe362
Timo Lehtola <timo.lehtola@saunalahti.fi>
2018-07-22 11:00:50 +0300

    app ready

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,21 @@
 <template>
     <div class="container">
+        <app-header :quoteCount="quotes.length" :maxQuotes="maxQuotes"></app-header>
         <app-new-quote @quoteAdded="newQuote"></app-new-quote>
-        <app-quote-grid :quotes="quotes"></app-quote-grid>
+        <app-quote-grid :quotes="quotes" @quoteDeleted="deleteQuote"></app-quote-grid>
+        <div class="row">
+            <div class="col-sm-12 text-center">
+                <div class="alert alert-info">Info: Click on Quote to delete it!</div>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
 import QuoteGrid from './components/QuoteGrid.vue'
 import NewQuote from './components/NewQuote.vue'
+import Header from './components/Header.vue'
+
     export default {
         data: function() {
             return {
@@ -17,11 +25,20 @@ import NewQuote from './components/NewQuote.vue'
         },
         components: {
             appQuoteGrid: QuoteGrid,
-            appNewQuote: NewQuote
+            appNewQuote: NewQuote,
+            appHeader: Header
         },
         methods: {
-            newQuote(event) {
-                this.quotes.push(event)
+            newQuote(quote) {
+                if (this.quotes.length >= this.maxQuotes) {
+                    alert('Please delete Quote to add new one')
+                } else {
+                    this.quotes.push(quote)
+                }
+
+            },
+            deleteQuote(index) {
+                this.quotes.splice(index, 1);
             }
         }
     }
