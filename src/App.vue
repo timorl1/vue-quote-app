3b39df9b588a42b3e4a64204d3d85a87c0d8600b
Timo Lehtola <timo.lehtola@saunalahti.fi>
2018-07-22 10:34:52 +0300

    added functionality to add quotes

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,29 @@
 <template>
     <div class="container">
-
+        <app-new-quote @quoteAdded="newQuote"></app-new-quote>
+        <app-quote-grid :quotes="quotes"></app-quote-grid>
     </div>
 </template>
 
 <script>
+import QuoteGrid from './components/QuoteGrid.vue'
+import NewQuote from './components/NewQuote.vue'
     export default {
-
+        data: function() {
+            return {
+                quotes: [],
+                maxQuotes: 10
+            }
+        },
+        components: {
+            appQuoteGrid: QuoteGrid,
+            appNewQuote: NewQuote
+        },
+        methods: {
+            newQuote(event) {
+                this.quotes.push(event)
+            }
+        }
     }
 </script>
 
